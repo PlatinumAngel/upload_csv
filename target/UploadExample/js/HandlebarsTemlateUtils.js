4f494d2ed23c3011f2142898b1a11f53e16d1292
git <mtg.mtg@o2.pl>
2019-03-03 12:45:37 +0100

    fix version

--- target/UploadExample/js/HandlebarsTemlateUtils.js
+++
@@ -1,5 +0,0 @@
-function fillTemplate(templateName, targetAreaId, content) {
-    var source   = $(templateName).html();
-    var template = Handlebars.compile(source);
-    $(targetAreaId).html(template(content));
-}
